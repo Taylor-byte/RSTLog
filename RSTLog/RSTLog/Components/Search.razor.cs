4e9d0a868004dc9fb8fd189a8c54d47722010cd3
Jack Taylor <jack.taylor10@outlook.com>
2021-03-27 15:33:13 +0000

    Search timer
    Timer to wait for search typeing to stop to send search reqest to the api
    If this wasnt implimented then search request would be made on every key press, increasing app load.

--- RSTLog/RSTLog/Components/Search.razor.cs
+++ RSTLog/RSTLog/Components/Search.razor.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace RSTLog.Components
@@ -10,9 +11,25 @@ namespace RSTLog.Components
     {
         public string SearchTerm { get; set; }
 
+        private Timer _timer;
+
         [Parameter]
         public EventCallback<string> OnSearchChanged { get; set; }
 
+        private void SearchChanged()
+        {
+            if (_timer != null)
+                _timer.Dispose();
 
+            _timer = new Timer(OnTimerElapsed, null, 500, 0);
+        }
+        //Timer to wait for search typeing to stop to send search reqest to the api
+        //If this wasnt implimented then search request would be made on every key press, increasing app load. 
+        private void OnTimerElapsed(object sender)
+        {
+            OnSearchChanged.InvokeAsync(SearchTerm);
+
+            _timer.Dispose();
+        }
     }
 }
