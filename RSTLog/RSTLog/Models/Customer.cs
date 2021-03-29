4de4bf25cf0b9612b4f85b415c5352c8eca8d108
Jack Taylor <jack.taylor10@outlook.com>
2021-03-29 21:05:50 +0100

    Create form validation

--- RSTLog/RSTLog/Models/Customer.cs
+++ RSTLog/RSTLog/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,19 +10,22 @@ namespace RSTLog.Models
     {
 
         public int Id { get; set; }
+        [Required(ErrorMessage = "Name Field is required")]
         public string Name { get; set; }
 
         public int? HoursPurchased { get; set; }
 
         public int? HoursRemaining { get; set; }
 
-        public DateTime Invoiced { get; set; }
+        public DateTime? Invoiced { get; set; }
 
         public string Notes { get; set; }
 
         public int? OnsitePurchased { get; set; }
 
         public bool Paid { get; set; }
+        
+        public string InvoicedDate { get; set; }
 
     }
 }
